1e89d3153c36efb48ac108e4b4e398a1d51f1fec
franzromanov <114895290+franzromanov@users.noreply.github.com>
2024-06-15 23:06:16 +0800

    Delete 328p directory

--- 328p/ANALOG/analog_1.sh
+++
@@ -1,3 +0,0 @@
-avr-gcc -g -nostartfiles -mmcu=atmega328p -DF_CPU=16000000UL -o analog_1.elf analog_1.s 
-avr-objcopy -O ihex -R .eeprom analog_1.elf analog_1.hex 
-avrdude -c arduino -p m328p -P /dev/ttyUSB* -b 115200 -U flash:w:analog_1.hex
